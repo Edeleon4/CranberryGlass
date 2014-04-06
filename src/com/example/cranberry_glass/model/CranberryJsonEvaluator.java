7269e91fdfc137402e1ced1b03f22b89094d2301
Ido-Efrati <idoe@mit.edu>
2014-04-06 17:05:13 -0400

    moving methods to an evaluator class

--- src/com/example/cranberry_glass/model/CranberryJsonEvaluator.java
+++ src/com/example/cranberry_glass/model/CranberryJsonEvaluator.java
@@ -1,5 +1,54 @@
 package com.example.cranberry_glass.model;
 
-public class CranberryJsonEvaluator {
+import java.io.IOException;
 
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CranberryJsonEvaluator {
+	String siteURL;
+	final CranberryServerManager server = new CranberryServerManager();
+
+
+	public CranberryJsonEvaluator(String url) {
+		siteURL = url;
+	}
+	/**
+	 * Given a siteURL get the URL to site's devices. Each device contains a list of sensors
+	 * @return
+	 * @throws IOException
+	 * @throws JSONException
+	 */
+	public String getSiteJSON() throws IOException, JSONException {
+		try {
+			JSONObject siteJson = server.getJSONFromURL(siteURL);
+    		JSONObject siteDevicesValues = (JSONObject) siteJson.get("devices");
+    		String siteDevicesLink = siteDevicesValues.get("_href").toString();
+			return siteDevicesLink;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	
+	public void getListOfNodes(String siteDevicesURL){
+		try {
+			JSONObject siteJson = server.getJSONFromURL(siteDevicesURL);
+			System.out.println("$$$$$$@#@#@$$");
+			System.out.println(siteJson);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+	}
 }
