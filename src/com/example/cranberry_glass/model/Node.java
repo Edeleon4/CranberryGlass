073532095a0d11f1901d8382656bebef0415b548
Ido-Efrati <idoe@mit.edu>
2014-04-07 14:02:00 -0400

    Adding Node class

--- src/com/example/cranberry_glass/model/Node.java
+++ src/com/example/cranberry_glass/model/Node.java
@@ -1,12 +1,22 @@
-/*package com.example.cranberry_glass.model;
+package com.example.cranberry_glass.model;
 
 import java.util.ArrayList;
 
 public class Node {
 	String nodeName;
 	ArrayList<Sensor> sensorsArray;
-	
-	public Node(String nodeName)
-	
-	
-}*/
+
+	public Node(String nodeName, ArrayList<Sensor> sensorArray) {
+		this.nodeName = nodeName;
+		this.sensorsArray = sensorArray;
+	}
+
+	public String getNodeName() {
+		return nodeName;
+	}
+
+	public ArrayList<Sensor> getSensorsArray() {
+		return sensorsArray;
+	}
+
+}
